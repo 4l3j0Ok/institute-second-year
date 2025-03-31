6e623fac0581defc02d99c27b5a1a6c3d1379154
4l3j0Ok <alejofsarmiento@gmail.com>
2025-03-30 23:51:56 -0300

    chore: agregado de variables de base de datos

--- programacion-orientada-a-objetos/3-clases-con-bbdd-pero-sin-bbdd-pero-igual-le-meto-bbdd/Variables.cs
+++ programacion-orientada-a-objetos/3-clases-con-bbdd-pero-sin-bbdd-pero-igual-le-meto-bbdd/Variables.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace _3_clases_con_bbdd_pero_sin_bbdd_pero_igual_le_meto_bbdd
 {
     internal class Variables
     {
+        public Variables()
+        {
+            string DBHost = "localhost";
+            string DBDatabse = "master";
+            string DBPassword = "pass";
+            string DBUsername = "sa";
+            string connectionString = $"Server={DBHost};Database={DBDatabse};User Id={DBUsername};Password={DBPassword};";
+        }
     }
 }
