f5116862d395bfaf35f5fcf062894e6f481306eb
4l3j0Ok <alejofsarmiento@gmail.com>
2025-04-05 16:20:45 -0300

    refactor: actualizar estructura del proyecto, estableciendo el estilo en el layout.

---
+++ projects-browser/src/colors.ts
@@ -0,0 +1,7 @@
+export class Colors {
+    static accent = "#8f002b"
+    static accentHover = "#924d62"
+    static background = "#eee"
+    static error = "#ff3a3a"
+    static font = "#ffffff"
+}
--- projects-browser/src/consts.ts
+++ projects-browser/src/consts.ts
@@ -1,5 +1,8 @@
+export const GITHUB_URL = "https://github.com/4l3j0Ok"
+export const LINKEDIN_URL = "https://www.linkedin.com/in/alejoide"
+export const EMAIL = "alejofsarmiento@gmail.com"
 export const PROJECT_NAME = "Alejoide's Projects Browser"
 export const REPO_NAME = "institute-second-year";
-export const REPO_URL = `https://github.com/4l3j0Ok/${REPO_NAME}`;
+export const REPO_URL = `${GITHUB_URL}/${REPO_NAME}`;
 export const SITE_NAME = `${PROJECT_NAME} - ${REPO_NAME}`;
 export const SITE_DESCRIPTION = `Lista de proyectos del repositorio ${REPO_NAME}`;
