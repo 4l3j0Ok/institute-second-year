37ebd47b4502a18ecb57d2193a12e603965198cd
4l3j0Ok <alejofsarmiento@gmail.com>
2025-04-11 22:01:32 -0300

    fix: corregir los diagramas y agregar queries

---
+++ base-de-datos/02-ejercicios/02-empresa/queries/03-insert-fields.sql
@@ -0,0 +1,6 @@
+-- Insertar datos --
+USE Empresa;
+INSERT INTO Clientes (nombre, apellido, correo)
+VALUES ('Pedro', 'Gomez', 'pgomez@argentina.gob.ar'),
+    ('Juan', 'Díaz', 'jdiaz@leafnoise.io'),
+    ('Julián', 'Weich', 'jweich@telefe.com.ar');
