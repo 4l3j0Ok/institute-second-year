e0883100aa13aa6df25c003719a8595fc8d845ed
Alejoide <alejofsarmiento@gmail.com>
2025-04-30 00:33:38 -0300

    feat: reorganize car catalog structure and styles, remove unused files

--- edi/02-concesionaria/modules/cars.js
+++ edi/02-concesionaria/modules/cars.js
@@ -1,30 +1,26 @@
-import { cars } from "../catalog.js";
+import { cars } from "./catalog.js";
 
 
 function displayCar() {
     const container = document.querySelector(".car");
-    container.innerHTML = "";
     const fileName = window.location.pathname.split('/').pop();
     const carId = fileName.replace('.html', '');
     cars.forEach(car => {
         if (car.id != carId) { return; }
-        const div = document.createElement("div")
         let price = `<b>Precio</b>: ${car.price}`;
         if (car.promotionPrice !== undefined) {
             price = `<b>Precio</b>: <span style="color:red;"><s>$${car.price}</s></span> $${car.promotionPrice}`;
         }
-        div.innerHTML = `
-            <section class="car-info">
-            <div class="car-image">
-                <img src="${car.img}" alt="Ford Ka">
-            </div>
-            <div class="car-actions">
-                <h1>${car.brand} ${car.model}</h1>
-                <p>Precio: ${price}</p>
-            </div>
+        container.innerHTML = `
+        <section class="car-presentation">
+            <img src="${car.img.replace('./assets', '../assets')}" alt="Ford Ka">
         </section>
         <section class="car-details">
             <h2>Detalles del auto</h2>
+            <div class="car-actions">
+                <h1>${car.brand} ${car.model}</h1>
+                <p>${price}</p>
+            </div>
             <p>Descripción: ${car.description}</p>
             <p>Kilometraje: ${car.km}</p>
         </section>
---
+++ edi/02-concesionaria/modules/catalog.js
@@ -0,0 +1,58 @@
+export const cars = [
+    {
+        id: "fiat-uno",
+        brand: "Fiat",
+        model: "Uno",
+        description: "Tempor do quis incididunt proident ipsum labore eiusmod. Minim irure laborum irure laboris velit mollit veniam exercitation. Consequat est excepteur aliqua ullamco aute quis. Pariatur dolore fugiat ullamco enim Lorem excepteur qui in amet eu aliqua laborum enim nostrud. Non officia est quis veniam dolor Lorem cillum. Esse nulla eiusmod ad duis non enim. Sit adipisicing qui est ea velit. Commodo ad est velit fugiat nulla non tempor proident reprehenderit officia enim id eu sint.Ullamco deserunt ex do esse.In labore proident do aliquip nostrud nostrud do cupidatat.Id sint mollit duis magna esse duis.Excepteur ullamco sunt proident Lorem occaecat velit non officia consectetur.Consequat velit esse eiusmod aliquip sint ipsum occaecat occaecat incididunt cillum velit ad. Ea aliquip mollit nulla fugiat voluptate deserunt voluptate veniam.Elit velit excepteur nostrud nulla in labore.Excepteur elit eu pariatur veniam consequat nostrud velit laborum anim elit.Incididunt ipsum mollit duis elit esse aliquip et laboris cupidatat sit id eu.Ipsum occaecat magna aliqua est duis et consectetur.Sint labore officia mollit veniam commodo enim qui ipsum consequat fugiat cillum.Consequat minim dolor nisi aute exercitation eu eiusmod laboris. Ullamco proident aliquip reprehenderit aliquip voluptate est qui labore et.Proident do reprehenderit excepteur id officia nisi eiusmod mollit commodo est reprehenderit.Et do labore irure deserunt duis aliquip minim anim.Cupidatat cupidatat id ad officia anim fugiat proident ut.Tempor dolore duis dolor commodo cillum non.Do ipsum nisi eiusmod cupidatat consequat.Cillum ipsum aliqua fugiat proident laborum tempor in in nostrud ipsum officia fugiat. Officia reprehenderit cillum reprehenderit id aliqua.Reprehenderit ex adipisicing laboris quis irure incididunt sunt pariatur enim cillum ea enim quis sit.Aliquip irure exercitation sit voluptate et tempor exercitation.Nostrud incididunt reprehenderit qui cillum est occaecat sit tempor.",
+        price: 10000000,
+        promotionPrice: 9000000,
+        km: 20,
+        year: 2020,
+        img: "./assets/images/fiat-uno.webp"
+    },
+    {
+        id: "ford-ka",
+        brand: "Ford",
+        model: "Ka",
+        description: "Tempor do quis incididunt proident ipsum labore eiusmod. Minim irure laborum irure laboris velit mollit veniam exercitation. Consequat est excepteur aliqua ullamco aute quis. Pariatur dolore fugiat ullamco enim Lorem excepteur qui in amet eu aliqua laborum enim nostrud. Non officia est quis veniam dolor Lorem cillum. Esse nulla eiusmod ad duis non enim. Sit adipisicing qui est ea velit. Commodo ad est velit fugiat nulla non tempor proident reprehenderit officia enim id eu sint.Ullamco deserunt ex do esse.In labore proident do aliquip nostrud nostrud do cupidatat.Id sint mollit duis magna esse duis.Excepteur ullamco sunt proident Lorem occaecat velit non officia consectetur.Consequat velit esse eiusmod aliquip sint ipsum occaecat occaecat incididunt cillum velit ad. Ea aliquip mollit nulla fugiat voluptate deserunt voluptate veniam. Elit velit excepteur nostrud nulla in labore.Excepteur elit eu pariatur veniam consequat nostrud velit laborum anim elit.Incididunt ipsum mollit duis elit esse aliquip et laboris cupidatat sit id eu.Ipsum occaecat magna aliqua est duis et consectetur.Sint labore officia mollit veniam commodo enim qui ipsum consequat fugiat cillum.Consequat minim dolor nisi aute exercitation eu eiusmod laboris. Ullamco proident aliquip reprehenderit aliquip voluptate est qui labore et. Proident do reprehenderit excepteur id officia nisi eiusmod mollit commodo est reprehenderit.Et do labore irure deserunt duis aliquip minim anim.Cupidatat cupidatat id ad officia anim fugiat proident ut.Tempor dolore duis dolor commodo cillum non.Do ipsum nisi eiusmod cupidatat consequat.Cillum ipsum aliqua fugiat proident laborum tempor in in nostrud ipsum officia fugiat. Officia reprehenderit cillum reprehenderit id aliqua.Reprehenderit ex adipisicing laboris quis irure incididunt sunt pariatur enim cillum ea enim quis sit.Aliquip irure exercitation sit voluptate et tempor exercitation.Nostrud incididunt reprehenderit qui cillum est occaecat sit tempor.",
+        price: 15000000,
+        promotionPrice: 14000000,
+        km: 0,
+        year: 2020,
+        img: "./assets/images/ford-ka.webp"
+    },
+    {
+        id: "chevrolet-onix",
+        brand: "Chevrolet",
+        model: "Onix",
+        description: "Tempor do quis incididunt proident ipsum labore eiusmod. Minim irure laborum irure laboris velit mollit veniam exercitation. Consequat est excepteur aliqua ullamco aute quis. Pariatur dolore fugiat ullamco enim Lorem excepteur qui in amet eu aliqua laborum enim nostrud. Non officia est quis veniam dolor Lorem cillum. Esse nulla eiusmod ad duis non enim. Sit adipisicing qui est ea velit. Commodo ad est velit fugiat nulla non tempor proident reprehenderit officia enim id eu sint.Ullamco deserunt ex do esse.In labore proident do aliquip nostrud nostrud do cupidatat.Id sint mollit duis magna esse duis.Excepteur ullamco sunt proident Lorem occaecat velit non officia consectetur.Consequat velit esse eiusmod aliquip sint ipsum occaecat occaecat incididunt cillum velit ad. Ea aliquip mollit nulla fugiat voluptate deserunt voluptate veniam.Elit velit excepteur nostrud nulla in labore.Excepteur elit eu pariatur veniam consequat nostrud velit laborum anim elit.Incididunt ipsum mollit duis elit esse aliquip et laboris cupidatat sit id eu.Ipsum occaecat magna aliqua est duis et consectetur.Sint labore officia mollit veniam commodo enim qui ipsum consequat fugiat cillum.Consequat minim dolor nisi aute exercitation eu eiusmod laboris. Ullamco proident aliquip reprehenderit aliquip voluptate est qui labore et.Proident do reprehenderit excepteur id officia nisi eiusmod mollit commodo est reprehenderit.Et do labore irure deserunt duis aliquip minim anim.Cupidatat cupidatat id ad officia anim fugiat proident ut.Tempor dolore duis dolor commodo cillum non.Do ipsum nisi eiusmod cupidatat consequat.Cillum ipsum aliqua fugiat proident laborum tempor in in nostrud ipsum officia fugiat. Officia reprehenderit cillum reprehenderit id aliqua.Reprehenderit ex adipisicing laboris quis irure incididunt sunt pariatur enim cillum ea enim quis sit.Aliquip irure exercitation sit voluptate et tempor exercitation.Nostrud incididunt reprehenderit qui cillum est occaecat sit tempor.",
+        price: 20000000,
+        promotionPrice: 19000000,
+        km: 0,
+        year: 2020,
+        img: "./assets/images/chevrolet-onix.webp"
+    }
+];
+
+export function displayCars() {
+    const container = document.querySelector(".catalog");
+    container.innerHTML = "";
+    cars.forEach(car => {
+        const div = document.createElement("div");
+        let price = `<b>Precio</b>: ${car.price}`;
+        let endpoint = `./cars/${car.brand.toLowerCase()}-${car.model.toLowerCase()}.html`;
+        if (car.promotionPrice !== undefined) {
+            price = `<b>Precio</b>: <span style="color:red;"><s>$${car.price}</s></span> $${car.promotionPrice}`;
+        }
+        div.innerHTML = `
+            <a href="${endpoint}" class="car-link">
+                <div class="car" id="${car.brand}-${car.model}">
+                    <img src="${car.img}" alt="${car.brand} ${car.model}">
+                    <h2>${car.brand} ${car.model}</h2>
+                    <p class="price">${price}</p>
+                </div>
+            </a>
+        `;
+        container.appendChild(div);
+    });
+}
