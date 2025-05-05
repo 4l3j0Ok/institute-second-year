a8202f9342044c989bde043a115e6b23a5edf339
4l3j0Ok <alejofsarmiento@gmail.com>
2025-05-05 01:29:27 -0300

    feat: agregar nuevos estilos y logica de display de autos

--- edi/02-concesionaria/modules/cars.js
+++ edi/02-concesionaria/modules/cars.js
@@ -7,24 +7,30 @@ export function displayCar() {
     const carId = fileName.replace('.html', '');
     cars.forEach(car => {
         if (car.id != carId) { return; }
-        let price = `<b>Precio</b>: ${car.price}`;
+        let price = `<span style="color: green">${car.price}</span>`;
         if (car.promotionPrice !== undefined) {
-            price = `<b>Precio</b>: <span style="color:red;"><s>$${car.price}</s></span> $${car.promotionPrice}`;
+            price = `<span style="color:red;">
+                        <s>$${car.price}</s>
+                    </span>
+                    <br>
+                    <b><span style="color: green; font-size: 1.3rem">
+                        $${car.promotionPrice}
+                    </span></b>`;
         }
         container.innerHTML = `
-        <section class="car-presentation">
-            <img src="${car.img.replace('./assets', '../assets')}" alt="Ford Ka">
-        </section>
-        <section class="car-details">
-            <h2>Detalles del auto</h2>
-            <div class="car-actions">
+        <section class="car-header">
+            <section class="car-presentation">
+                <img src="${car.img.replace('./assets', '../assets')}" alt="Ford Ka">
+            </section>
+            <section class="car-info">
                 <h1>${car.brand} ${car.model}</h1>
+                <p><b>${car.km}km</b></p>
                 <p>${price}</p>
-            </div>
-            <p>Descripción: ${car.description}</p>
-            <p>Kilometraje: ${car.km}</p>
+                </section>
+        </section>
+        <section class="car-description">
+            <p>${car.description}</p>
         </section>
         `;
-        container.appendChild(div);
     })
 }
