63dcd3d794c36f471e01bdb11a8902019206f586
4l3j0Ok <alejofsarmiento@gmail.com>
2025-05-26 17:28:19 -0300

    Reapply "feat: agregar nuevo formulario de cine"
    This reverts commit 6b29b589fff04a6fec05f6b93fe02b74aa98eccf.

---
+++ seminario-de-programacion/05-cine/MainForm.cs
@@ -0,0 +1,22 @@
+using MaterialSkin.Controls;
+
+namespace _05_cine
+{
+    public partial class MainForm : MaterialForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
