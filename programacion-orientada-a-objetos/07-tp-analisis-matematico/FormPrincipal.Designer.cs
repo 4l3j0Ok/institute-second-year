15a46436f1f31211816924b2917692b51e0e5bd5
4l3j0Ok <alejofsarmiento@gmail.com>
2025-07-11 18:15:25 -0300

    Agregar panel y etiqueta para coeficientes en el formulario principal

--- programacion-orientada-a-objetos/07-tp-analisis-matematico/FormPrincipal.Designer.cs
+++ programacion-orientada-a-objetos/07-tp-analisis-matematico/FormPrincipal.Designer.cs
@@ -34,9 +34,12 @@
             tableLayoutPanel1 = new TableLayoutPanel();
             tableLayoutPanel2 = new TableLayoutPanel();
             btnDerivar = new ReaLTaiizor.Controls.HopeButton();
+            panel1 = new Panel();
             tbCoeficientes = new ReaLTaiizor.Controls.BigTextBox();
+            label1 = new Label();
             tableLayoutPanel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // hopeFormPrincipal
@@ -50,7 +53,7 @@
             hopeFormPrincipal.Image = (Image)resources.GetObject("hopeFormPrincipal.Image");
             hopeFormPrincipal.Location = new Point(0, 0);
             hopeFormPrincipal.Name = "hopeFormPrincipal";
-            hopeFormPrincipal.Size = new Size(767, 40);
+            hopeFormPrincipal.Size = new Size(700, 40);
             hopeFormPrincipal.TabIndex = 0;
             hopeFormPrincipal.Text = "PoliFórmico";
             hopeFormPrincipal.ThemeColor = Color.FromArgb(64, 0, 64);
@@ -61,7 +64,7 @@
             plotView.Location = new Point(3, 3);
             plotView.Name = "plotView";
             plotView.PanCursor = Cursors.Hand;
-            plotView.Size = new Size(761, 455);
+            plotView.Size = new Size(694, 384);
             plotView.TabIndex = 1;
             plotView.Text = "plotView1";
             plotView.ZoomHorizontalCursor = Cursors.SizeWE;
@@ -79,8 +82,8 @@
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 2;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
-            tableLayoutPanel1.Size = new Size(767, 511);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 70F));
+            tableLayoutPanel1.Size = new Size(700, 460);
             tableLayoutPanel1.TabIndex = 2;
             // 
             // tableLayoutPanel2
@@ -89,13 +92,13 @@
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80F));
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
             tableLayoutPanel2.Controls.Add(btnDerivar, 1, 0);
-            tableLayoutPanel2.Controls.Add(tbCoeficientes, 0, 0);
+            tableLayoutPanel2.Controls.Add(panel1, 0, 0);
             tableLayoutPanel2.Dock = DockStyle.Fill;
-            tableLayoutPanel2.Location = new Point(3, 464);
+            tableLayoutPanel2.Location = new Point(3, 393);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 1;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel2.Size = new Size(761, 44);
+            tableLayoutPanel2.Size = new Size(694, 64);
             tableLayoutPanel2.TabIndex = 2;
             // 
             // btnDerivar
@@ -106,12 +109,13 @@
             btnDerivar.DefaultColor = Color.FromArgb(255, 255, 255);
             btnDerivar.Dock = DockStyle.Fill;
             btnDerivar.Font = new Font("Segoe UI", 12F);
-            btnDerivar.HoverTextColor = Color.FromArgb(48, 49, 51);
+            btnDerivar.HoverTextColor = Color.White;
             btnDerivar.InfoColor = Color.FromArgb(144, 147, 153);
-            btnDerivar.Location = new Point(611, 3);
+            btnDerivar.Location = new Point(558, 20);
+            btnDerivar.Margin = new Padding(3, 20, 3, 3);
             btnDerivar.Name = "btnDerivar";
-            btnDerivar.PrimaryColor = Color.FromArgb(64, 158, 255);
-            btnDerivar.Size = new Size(147, 38);
+            btnDerivar.PrimaryColor = Color.FromArgb(64, 0, 64);
+            btnDerivar.Size = new Size(133, 41);
             btnDerivar.SuccessColor = Color.FromArgb(103, 194, 58);
             btnDerivar.TabIndex = 6;
             btnDerivar.Text = "Derivar";
@@ -119,29 +123,49 @@
             btnDerivar.WarningColor = Color.FromArgb(230, 162, 60);
             btnDerivar.Click += btnDerivar_Click;
             // 
+            // panel1
+            // 
+            panel1.Controls.Add(tbCoeficientes);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(549, 58);
+            panel1.TabIndex = 7;
+            // 
             // tbCoeficientes
             // 
             tbCoeficientes.BackColor = Color.Transparent;
-            tbCoeficientes.Dock = DockStyle.Fill;
+            tbCoeficientes.Dock = DockStyle.Top;
             tbCoeficientes.Font = new Font("Tahoma", 11F);
             tbCoeficientes.ForeColor = Color.DimGray;
             tbCoeficientes.Image = null;
-            tbCoeficientes.Location = new Point(3, 3);
+            tbCoeficientes.Location = new Point(0, 15);
             tbCoeficientes.MaxLength = 32767;
             tbCoeficientes.Multiline = false;
             tbCoeficientes.Name = "tbCoeficientes";
             tbCoeficientes.ReadOnly = false;
-            tbCoeficientes.Size = new Size(602, 41);
-            tbCoeficientes.TabIndex = 5;
+            tbCoeficientes.Size = new Size(549, 41);
+            tbCoeficientes.TabIndex = 6;
             tbCoeficientes.TextAlignment = HorizontalAlignment.Center;
             tbCoeficientes.UseSystemPasswordChar = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Top;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Coeficientes";
             // 
             // FormPrincipal
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            ClientSize = new Size(767, 551);
+            ClientSize = new Size(700, 500);
             Controls.Add(tableLayoutPanel1);
             Controls.Add(hopeFormPrincipal);
             FormBorderStyle = FormBorderStyle.None;
@@ -154,6 +178,8 @@
             Load += FormPrincipal_Load;
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel2.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -164,6 +190,8 @@
         private TableLayoutPanel tableLayoutPanel1;
         private TableLayoutPanel tableLayoutPanel2;
         private ReaLTaiizor.Controls.HopeButton btnDerivar;
+        private Panel panel1;
         private ReaLTaiizor.Controls.BigTextBox tbCoeficientes;
+        private Label label1;
     }
 }
